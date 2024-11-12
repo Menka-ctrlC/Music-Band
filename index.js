f19837ece80215c6c0c43b3f7928fb4207937800
Balram <balram24das@gmail.com>
2024-11-12 19:00:14 +0530

    Updated sound files and fixed button functionality

--- index.js
+++ index.js
@@ -18,37 +18,37 @@ function makeSound(key){
 switch (key) {
 
   case "q":
-var drum1 = new Audio('C:/Users/Balram/Desktop/menka/Music Band/sounds/drum-sounds/tom-1.mp3');
+var drum1 = new Audio('https://raw.githubusercontent.com/Menka-ctrlC/Music-Band/main/sounds/drum-sounds/tom-1.mp3');
  drum1.play();
  break;
         
 case "w":
-var drum2 = new Audio('C:/Users/Balram/Desktop/menka/Music Band/sounds/drum-sounds/tom-2.mp3');
+var drum2 = new Audio('https://raw.githubusercontent.com/Menka-ctrlC/Music-Band/main/sounds/drum-sounds/tom-2.mp3');
 drum2.play();
  break;
 
  case "e":
- var drum3 = new Audio('C:/Users/Balram/Desktop/menka/Music Band/sounds/drum-sounds/tom-3.mp3');
+ var drum3 = new Audio('https://raw.githubusercontent.com/Menka-ctrlC/Music-Band/main/sounds/drum-sounds/tom-3.mp3');
  drum3.play();
  break;
 
  case "r":
-var drum4 = new Audio('C:/Users/Balram/Desktop/menka/Music Band/sounds/drum-sounds/tom-4.mp3');
+var drum4 = new Audio('https://raw.githubusercontent.com/Menka-ctrlC/Music-Band/main/sounds/drum-sounds/tom-4.mp3');
  drum4.play();
   break;
 
   case "u":
- var drum5 = new Audio('C:/Users/Balram/Desktop/menka/Music Band/sounds/drum-sounds/snare.mp3');
+ var drum5 = new Audio('https://raw.githubusercontent.com/Menka-ctrlC/Music-Band/main/sounds/drum-sounds/snare.mp3');
  drum5.play();
 break;
 
   case "i":
- var drum6 = new Audio('C:/Users/Balram/Desktop/menka/Music Band/sounds/drum-sounds/kick-bass.mp3');
+ var drum6 = new Audio('https://raw.githubusercontent.com/Menka-ctrlC/Music-Band/main/sounds/drum-sounds/kick-bass.mp3');
  drum6.play();
   break;
 
    case "o":
-  var drum7 = new Audio('C:/Users/Balram/Desktop/menka/Music Band/sounds/drum-sounds/crash.mp3');
+  var drum7 = new Audio('https://raw.githubusercontent.com/Menka-ctrlC/Music-Band/main/sounds/drum-sounds/crash.mp3');
   drum7.play();
      break; 
         default:
@@ -89,32 +89,32 @@ for(var g = 0; g < guitarLength; g++){
 
       switch (key) {
           case "a":
-          var guitar1 = new Audio("C:/Users/Balram/Desktop/menka/Music Band/sounds/guitar-sounds/guitar1.mp3")
+          var guitar1 = new Audio("https://raw.githubusercontent.com/Menka-ctrlC/Music-Band/main/sounds/guitar-sounds/guitar1.mp3")
           guitar1.play();
           break;
 
           case "s":
-            var guitar2 = new Audio("C:/Users/Balram/Desktop/menka/Music Band/sounds/guitar-sounds/guitar2.mp3")
+            var guitar2 = new Audio("https://raw.githubusercontent.com/Menka-ctrlC/Music-Band/main/sounds/guitar-sounds/guitar2.mp3")
             guitar2.play();
             break;
 
           case "d":
-             var guitar3 = new Audio("C:/Users/Balram/Desktop/menka/Music Band/sounds/guitar-sounds/guitar3.mp3")
+             var guitar3 = new Audio("https://raw.githubusercontent.com/Menka-ctrlC/Music-Band/main/sounds/guitar-sounds/guitar3.mp3")
              guitar3.play();
               break;
 
            case "f":
-              var guitar4 = new Audio("C:/Users/Balram/Desktop/menka/Music Band/sounds/guitar-sounds/guitar4.mp3")
+              var guitar4 = new Audio("https://raw.githubusercontent.com/Menka-ctrlC/Music-Band/main/sounds/guitar-sounds/guitar4.mp3")
               guitar4.play();
               break;  
 
            case "k":
-              var guitar5 = new Audio("C:/Users/Balram/Desktop/menka/Music Band/sounds/guitar-sounds/guitar5.mp3")
+              var guitar5 = new Audio("https://raw.githubusercontent.com/Menka-ctrlC/Music-Band/main/sounds/guitar-sounds/guitar5.mp3")
               guitar5.play();
               break;
                      
             case "l":
-               var guitar6 = new Audio("C:/Users/Balram/Desktop/menka/Music Band/sounds/guitar-sounds/guitar6.mp3")
+               var guitar6 = new Audio("https://raw.githubusercontent.com/Menka-ctrlC/Music-Band/main/sounds/guitar-sounds/guitar6.mp3")
                guitar6.play();
                break;
 
@@ -157,22 +157,22 @@ function pianoSound(key){
    
 switch (key) {
    case "z":
-   var piano1 = new Audio("C:/Users/Balram/Desktop/menka/Music Band/sounds/piano-sounds/piano1.mp3")
+   var piano1 = new Audio("https://raw.githubusercontent.com/Menka-ctrlC/Music-Band/main/sounds/piano-sounds/piano1.mp3")
    piano1.play()
    break;  case "x":
-   var piano2 = new Audio("C:/Users/Balram/Desktop/menka/Music Band/sounds/piano-sounds/piano2.mp3")
+   var piano2 = new Audio("https://raw.githubusercontent.com/Menka-ctrlC/Music-Band/main/sounds/piano-sounds/piano2.mp3")
    piano2.play()
    break;  case "c":
-   var piano3 = new Audio("C:/Users/Balram/Desktop/menka/Music Band/sounds/piano-sounds/piano3.mp3")
+   var piano3 = new Audio("https://raw.githubusercontent.com/Menka-ctrlC/Music-Band/main/sounds/piano-sounds/piano3.mp3")
    piano3.play()
    break;  case "v":
-   var piano4 = new Audio("C:/Users/Balram/Desktop/menka/Music Band/sounds/piano-sounds/piano4.mp3")
+   var piano4 = new Audio("https://raw.githubusercontent.com/Menka-ctrlC/Music-Band/main/sounds/piano-sounds/piano4.mp3")
    piano4.play()
    break;  case "n":
-   var piano5 = new Audio("C:/Users/Balram/Desktop/menka/Music Band/sounds/piano-sounds/piano5.mp3")
+   var piano5 = new Audio("https://raw.githubusercontent.com/Menka-ctrlC/Music-Band/main/sounds/piano-sounds/piano5.mp3")
    piano5.play()
    break;  case "m":
-   var piano6 = new Audio("C:/Users/Balram/Desktop/menka/Music Band/sounds/piano-sounds/piano6.mp3")
+   var piano6 = new Audio("https://raw.githubusercontent.com/Menka-ctrlC/Music-Band/main/sounds/piano-sounds/piano6.mp3")
    piano6.play()
  break;
   
